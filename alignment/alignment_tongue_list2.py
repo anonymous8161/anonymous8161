6357896e35d6a0ffd1f9725580118167226249a8
anonymous8161 <anonymous25_8161@outlook.com>
2024-08-18 15:00:27 +0800

    Update alignment_tongue_list2.py

--- alignment/alignment_tongue_list2.py
+++ alignment/alignment_tongue_list2.py
@@ -31,19 +31,19 @@ def process_images(input_folder, output_folder, output_with_points_folder, origi
 
                     aligned_image, original_tip, original_top, aligned_tip, aligned_top, _, _ = align_segmented_tongue(color_image)
                     
-                    # 创建带有关键点的对齐图像
+                   
                     aligned_with_points = aligned_image.copy()
-                    cv2.circle(aligned_with_points, aligned_tip, 5, (0, 0, 255), -1)  # 舌尖用红色标记
-                    cv2.circle(aligned_with_points, aligned_top, 5, (255, 255, 0), -1)  # 舌顶用青色标记
-                    cv2.line(aligned_with_points, aligned_tip, aligned_top, (255, 0, 255), 2)  # 连线用粉色标记
+                    cv2.circle(aligned_with_points, aligned_tip, 5, (0, 0, 255), -1)  
+                    cv2.circle(aligned_with_points, aligned_top, 5, (255, 255, 0), -1)  
+                    cv2.line(aligned_with_points, aligned_tip, aligned_top, (255, 0, 255), 2)  
 
-                    # 创建带有关键点的原始图像
+                 
                     original_with_points = color_image.copy()
-                    cv2.circle(original_with_points, original_tip, 5, (0, 0, 255), -1)  # 舌尖用红色标记
-                    cv2.circle(original_with_points, original_top, 5, (255, 255, 0), -1)  # 舌顶用青色标记
-                    cv2.line(original_with_points, original_tip, original_top, (255, 0, 255), 2)  # 连线用粉色标记
+                    cv2.circle(original_with_points, original_tip, 5, (0, 0, 255), -1)  
+                    cv2.circle(original_with_points, original_top, 5, (255, 255, 0), -1) 
+                    cv2.line(original_with_points, original_tip, original_top, (255, 0, 255), 2) 
 
-                    # 保存图像
+               
                     cv2.imwrite(output_path, aligned_image)
                     cv2.imwrite(output_with_points_path, aligned_with_points)
                     cv2.imwrite(original_with_points_path, original_with_points)
@@ -57,9 +57,9 @@ def process_images(input_folder, output_folder, output_with_points_folder, origi
     print(f"Processing completed. Total images: {total_count}, Successfully processed: {successful_count}")
 
 if __name__ == "__main__":
-    input_folder = "seg_cropped_images"  # 替换为您的输入文件夹路径
-    output_folder = "seg_cropped_images_align"  # 替换为您想要保存处理后图像的文件夹路径
-    output_with_points_folder = "seg_cropped_images_align_with_points"  # 替换为您想要保存带关键点图像的文件夹路径
-    original_with_points_folder = "seg_cropped_images_original_with_points"  # 替换为您想要保存带关键点原始图像的文件夹路径
+    input_folder = "seg_cropped_images"  
+    output_folder = "seg_cropped_images_align" 
+    output_with_points_folder = "seg_cropped_images_align_with_points"  
+    original_with_points_folder = "seg_cropped_images_original_with_points" 
     
     process_images(input_folder, output_folder, output_with_points_folder, original_with_points_folder)
